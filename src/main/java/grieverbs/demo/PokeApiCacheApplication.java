f79f4795a605d6eb021d714aca3b3edfef7a7ffd
John Ho <ho.john.nguyen@gmail.com>
2023-12-18 18:23:31 -0800

    Added the scaffolding for a project.

---
+++ src/main/java/grieverbs/demo/PokeApiCacheApplication.java
@@ -0,0 +1,13 @@
+package grieverbs.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PokeApiCacheApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PokeApiCacheApplication.class, args);
+	}
+
+}
---
+++ src/test/java/grieverbs/demo/PokeApiCacheApplicationTests.java
@@ -0,0 +1,13 @@
+package grieverbs.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PokeApiCacheApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
